b54296c5b8f7abae777c78a41dd2a4e2ebfc7ae7
Gleryston Matos <glerystonmatos@gmail.com>
2024-03-02 12:01:27 -0300

    Implementação dos exemplos em React JS de Criptografia com HMAC SHA1 e SHA256 hexadecimal e base64 e de Hash utilizando SHA1, SHA256, SHA384 e SHA512

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,8 +1,15 @@
-import md5 from 'md5';
+import md5 from "md5";
+import sha1 from "crypto-js/sha1";
+import sha256 from "crypto-js/sha256";
+import sha384 from "crypto-js/sha384";
+import sha512 from "crypto-js/sha512";
+import base64 from "crypto-js/enc-base64";
+import hmacSha1 from "crypto-js/hmac-sha1";
+import hmacSha256 from "crypto-js/hmac-sha256";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -19,15 +26,68 @@ function App() {
           rel="noopener noreferrer">
           Learn React
         </a>
-        <p>
-          Value: gleryston-matos
+        <p className="App-p">
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+          <br></br>
+          HMAC SHA1 Hex: {
+            "0x" + hmacSha1("gleryston-matos", "8dc115f4e2b640f6a2bc4046fdc5c635").toString().toUpperCase()
+          }<br></br>
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+          <br></br>
+          HMAC SHA256 Hex: {
+            "0x" + hmacSha256("gleryston-matos", "8dc115f4e2b640f6a2bc4046fdc5c635").toString().toUpperCase()
+          }<br></br>
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+          <br></br>
+          HMAC SHA1 Base64: {
+            base64.stringify(hmacSha1("gleryston-matos", "8dc115f4e2b640f6a2bc4046fdc5c635"))
+          }<br></br>
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+          <br></br>
+          HMAC SHA256 Base64: {
+            base64.stringify(hmacSha256("gleryston-matos", "8dc115f4e2b640f6a2bc4046fdc5c635"))
+          }<br></br>
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
           <br></br>
           MD5 01: {
-            md5('gleryston-matos').toUpperCase()
+            md5("gleryston-matos").toUpperCase()
           }<br></br>
           MD5 02: {
-            md5('gleryston-matos').toUpperCase()
-          }
+            md5("gleryston-matos").toUpperCase()
+          }<br></br>
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+          <br></br>
+          SHA1 01: {
+            sha1("gleryston-matos").toString().toUpperCase()
+          }<br></br>
+          SHA1 02: {
+            sha1("gleryston-matos").toString().toUpperCase()
+          }<br></br>
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+          <br></br>
+          SHA256 01: {
+            sha256("gleryston-matos").toString().toUpperCase()
+          }<br></br>
+          SHA256 02: {
+            sha256("gleryston-matos").toString().toUpperCase()
+          }<br></br>
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+          <br></br>
+          SHA384 01: {
+            sha384("gleryston-matos").toString().toUpperCase()
+          }<br></br>
+          SHA384 02: {
+            sha384("gleryston-matos").toString().toUpperCase()
+          }<br></br>
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+          <br></br>
+          SHA512 01: {
+            sha512("gleryston-matos").toString().toUpperCase()
+          }<br></br>
+          SHA512 02: {
+            sha512("gleryston-matos").toString().toUpperCase()
+          }<br></br>
+          ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         </p>
       </header>
     </div>
