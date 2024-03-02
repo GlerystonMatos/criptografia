d32087c2bd36cbea1bb2d81cffd5b8d44ae942fe
Gleryston Matos <glerystonmatos@gmail.com>
2024-03-02 07:39:49 -0300

    Implementação dos exemplos em C# de Hash utilizando SHA1, SHA256, SHA384 e SHA512

--- c#/Criptografia/Program.cs
+++ c#/Criptografia/Program.cs
@@ -7,22 +7,42 @@ namespace Criptografia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------");
             Console.Write("HMAC SHA1 Hex: ");
             Console.WriteLine(HMACSHA1HashValueHexStr("8dc115f4e2b640f6a2bc4046fdc5c635", "gleryston-matos"));
             Console.Write("HMAC SHA256 Hex: ");
             Console.WriteLine(HMACSHA256HashValueHexStr("8dc115f4e2b640f6a2bc4046fdc5c635", "gleryston-matos"));
-            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------");
             Console.Write("HMAC SHA1 Base64: ");
             Console.WriteLine(HMACSHA1HashValueBase64("8dc115f4e2b640f6a2bc4046fdc5c635", "gleryston-matos"));
             Console.Write("HMAC SHA256 Base64: ");
             Console.WriteLine(HMACSHA256HashValueBase64("8dc115f4e2b640f6a2bc4046fdc5c635", "gleryston-matos"));
-            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------");
             Console.Write("MD5 01: ");
             Console.WriteLine(MD5HashValue("gleryston-matos"));
             Console.Write("MD5 02: ");
             Console.WriteLine(MD5HashValue("gleryston-matos"));
-            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------");
+            Console.Write("SHA1 01: ");
+            Console.WriteLine(SHA1HashValue("gleryston-matos"));
+            Console.Write("SHA1 02: ");
+            Console.WriteLine(SHA1HashValue("gleryston-matos"));
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------");
+            Console.Write("SHA256 01: ");
+            Console.WriteLine(SHA256HashValue("gleryston-matos"));
+            Console.Write("SHA256 02: ");
+            Console.WriteLine(SHA256HashValue("gleryston-matos"));
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------");
+            Console.Write("SHA384 01: ");
+            Console.WriteLine(SHA384HashValue("gleryston-matos"));
+            Console.Write("SHA384 02: ");
+            Console.WriteLine(SHA384HashValue("gleryston-matos"));
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------");
+            Console.Write("SHA512 01: ");
+            Console.WriteLine(SHA512HashValue("gleryston-matos"));
+            Console.Write("SHA512 02: ");
+            Console.WriteLine(SHA512HashValue("gleryston-matos"));
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------");
         }
 
         private static byte[] BytesOf(string value)
@@ -96,10 +116,50 @@ namespace Criptografia
 
         private static string MD5HashValue(string value)
         {
-            MD5 md5 = MD5.Create();
+            MD5 hash = MD5.Create();
 
             byte[] inputBytes = Encoding.UTF8.GetBytes(value);
-            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            byte[] hashBytes = hash.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+
+        private static string SHA1HashValue(string value)
+        {
+            SHA1 hash = SHA1.Create();
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(value);
+            byte[] hashBytes = hash.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+
+        private static string SHA256HashValue(string value)
+        {
+            SHA256 hash = SHA256.Create();
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(value);
+            byte[] hashBytes = hash.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+
+        private static string SHA384HashValue(string value)
+        {
+            SHA384 hash = SHA384.Create();
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(value);
+            byte[] hashBytes = hash.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+
+        private static string SHA512HashValue(string value)
+        {
+            SHA512 hash = SHA512.Create();
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(value);
+            byte[] hashBytes = hash.ComputeHash(inputBytes);
 
             return Convert.ToHexString(hashBytes);
         }
